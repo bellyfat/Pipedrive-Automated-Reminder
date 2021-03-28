6182149378b0e870fb2dc2f15f09a12811436b01
n-winspear <50559711+n-winspear@users.noreply.github.com>
2021-03-29 06:56:44 +1300

    Updated to include a logger

--- index.py
+++ index.py
@@ -1,6 +1,8 @@
 import functions
 import mailer
 import os
+import datetime
+import json
 from dotenv import load_dotenv
 load_dotenv
 
@@ -29,12 +31,30 @@ def two_week_check():
         recipients = ['n.winspear@leadership.ac.nz']
         mail_handler_response = mail_handler.send_emails(mail_parts, subejct, recipients)
 
+    else:
+        mail_handler_response = {
+            "message": "No messages to send."
+        }
+
     return mail_handler_response
 
+def create_log(response: str) -> None:
+    try:
+        # Setup
+        log_folder_path = "/home/pi/Documents/Github/Pipedrive-Automated-Reminder/logs"
+        filename = datetime.datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")
+
+        # Writing to file
+        f = open("{}/{}.txt".format(log_folder_path, filename), "w")
+        f.write("Check performed at: {}\n\n{}".format(
+            datetime.datetime.now(), json.dumps(response.json(), indent=4, sort_keys=True)))
+        f.close()
+    except Exception as err:
+        print("Failed to create log \n", err)
 
 def main():
     response = two_week_check()
-    print(response)
+    create_log(response)
     
 
 main()
